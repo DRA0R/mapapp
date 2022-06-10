9cf52b62c72f2a435410d35793fecedd17ad3898
Draor <robson19990501@gmail.com>
2022-06-10 01:10:40 -0500

    Finish

--- mapapp/views.py
+++ mapapp/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from .models import Search
+from .forms import SearchForm
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 import folium
-
+import geocoder
 
 def index(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = SearchForm()
+
+    address = Search.objects.all().last()
+    location = geocoder.osm(address)
+    lat = location.lat
+    lng = location.lng
+    country = location.country
+
+    if lat == None or lng == None:
+        address.delete()
+        return HttpResponse('Your address input is invalid')
+
     #Create maps object
     m = folium.Map(location=[19,-12], zoom_start=2)
-    folium.Marker([5.594,-0.216], tooltip='Click for more',
-    popup='SALVADOR MORENO \n Tel: 3144300548').add_to(m)
+
+    ##point marker yourself
+    # folium.Marker([5.594,-0.216],
+    #  tooltip='Click for more', popup='Ghana').add_to(m)
+
+    folium.Marker([lat,lng],
+     tooltip='Click for more', popup=country).add_to(m)
     #get HTML representation of map 
     m = m._repr_html_()
     context = {
-        'm':m
+        'm':m,
+        'form':form,
     }
 
     return render(request, 'index.html', context)
